442731a9cea4b3d4315ae559dfb446a24c77f317
raine-lemon <raine@lemoncloud.io>
2024-10-30 17:37:47 +0900

    feat: add usecase interface - 유즈케이스 인터페이스 추가

---
+++ core-architecture/src/main/kotlin/io/lemoncloud/core/architecture/domain/SuspendUseCase.kt
@@ -0,0 +1,16 @@
+package io.lemoncloud.core.architecture.domain
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+/**
+ * 도메인 레이어의 유즈 케이스를 처리합니다.
+ */
+abstract class SuspendUseCase<In, Out>(private val coroutineDispatcher: CoroutineDispatcher) {
+
+    protected abstract suspend fun execute(params: In): Result<Out>
+
+    suspend operator fun invoke(parameters: In): Result<Out> {
+        return withContext(coroutineDispatcher) { execute(parameters) }
+    }
+}
