92e060fc03cc5f0f2fa70b0e6d443cd4fc974825
raine-lemon <raine@lemoncloud.io>
2024-10-30 18:01:33 +0900

    chore: configure core-util module - core-util 모듈 설정

---
+++ core-util/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.lemon.android.library)
+    alias(libs.plugins.lemon.android.feature)
+    alias(libs.plugins.lemon.android.hilt)
+    alias(libs.plugins.lemon.android.kotlin)
+    alias(libs.plugins.lemon.android.okhttp)
+    alias(libs.plugins.lemon.android.test.ext)
+}
+
+android {
+    namespace = "io.lemoncloud.core.util"
+}
