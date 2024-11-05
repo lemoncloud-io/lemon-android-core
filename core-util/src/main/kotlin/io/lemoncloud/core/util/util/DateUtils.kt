e77847662743dc1b1d86e9ca5504b0050338c985
raine-lemon <raine@lemoncloud.io>
2024-11-05 19:16:24 +0900

    feat: update DataUtils - 날짜를 문자열 포멧으로 변경하는 기능 수정 - 사용하지 않는 라이브러리 제거

--- core-util/src/main/kotlin/io/lemoncloud/core/util/util/DateUtils.kt
+++ core-util/src/main/kotlin/io/lemoncloud/core/util/util/DateUtils.kt
@@ -1,62 +1,34 @@
 package io.lemoncloud.core.util.util
 
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import java.util.TimeZone
+import android.os.Build
+import androidx.annotation.RequiresApi
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toJavaInstant
+import java.time.format.DateTimeFormatter
 
 object DateUtils {
 
     /**
-     * timeStamp 를 [Date]로 변경할 때 사용합니다.
-     * @param timeStamp 타임스탬프
+     * [Instant] 타입을 주어진 특정 날짜 포멧으로 변환할 때 사용합니다.
+     * @param format 변환 하고자 하는 날짜 스타일 포멧 (ex. yyyy-MM-dd)
      */
-    fun convertTimeStampToDate(timeStamp: Long): Date = Date(timeStamp)
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun Instant.toDateFormatString(
+        format: String,
+    ): String = DateTimeFormatter.ofPattern(format).format(toJavaInstant())
 
     /**
-     * Date 타입을 string format 으로 변환할 때 사용합니다.
-     * @param dateFormat 변환 하고자 하는 형식 (ex. yyyy-MM-dd, HH:mm:ss, yyyy-MM-dd HH:mm:ss, ...)
-     * @param timeZone 변환 하고자 하는 TimeZone (ex. Asia/Seoul)
+     * [LocalDateTime] 타입을 주어진 특정 날짜 포멧으로 변환할 때 사용합니다.
+     * @param format 변환 하고자 하는 날짜 스타일 포멧 (ex. yyyy-MM-dd)
+     * @param timeZone 사용 하고자 하는 TimeZone
      */
-    fun Date.toStringFormat(
-        dateFormat: String,
-        timeZone: String,
-        locale: Locale = Locale.getDefault()
-    ): String = SimpleDateFormat(dateFormat, locale).apply {
-        this.timeZone = TimeZone.getTimeZone(timeZone)
-    }.format(/* date = */ this)
-
-    /**
-     * Date 타입을 주어진 특정 날짜 포멧으로 변환할 때 사용합니다.
-     * @param dateStyle 변환 하고자 하는 날짜 스타일
-     * @param timeZone 변환 하고자 하는 TimeZone (ex. Asia/Seoul)
-     * @param locale 변환 하고자 하는 Locale
-     * @see DateStyle
-     */
-    fun Date.toStringFormat(
-        dateStyle: DateStyle,
-        timeZone: String,
-        locale: Locale = Locale.getDefault(),
-    ): String =
-        DateFormat.getDateInstance(
-            when (dateStyle) {
-                DateStyle.SHORT -> DateFormat.SHORT
-                DateStyle.MEDIUM -> DateFormat.MEDIUM
-                DateStyle.LONG -> DateFormat.LONG
-                DateStyle.FULL -> DateFormat.FULL
-            }, locale
-        ).apply {
-            this.timeZone = TimeZone.getTimeZone(timeZone)
-        }.format(/* date = */ this)
-
-    /**
-     * [DateFormat]의 날짜 포멧 상수 값과 연동하기 위한 타입
-     * @property DateStyle.SHORT  [DateFormat.SHORT]
-     * @property DateStyle.MEDIUM  [DateFormat.MEDIUM]
-     * @property DateStyle.LONG  [DateFormat.LONG]
-     * @property DateStyle.FULL  [DateFormat.FULL]
-     */
-    enum class DateStyle { SHORT, MEDIUM, LONG, FULL }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun LocalDateTime.toDateFormatString(
+        format: String,
+        timeZone: TimeZone = TimeZone.currentSystemDefault()
+    ): String = DateTimeFormatter.ofPattern(format).format(toInstant(timeZone).toJavaInstant())
 }
 
