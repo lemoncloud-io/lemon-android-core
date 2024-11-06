76ac09029a59cd7c885a3b98a7b9200126911539
raine-lemon <raine@lemoncloud.io>
2024-11-06 13:47:56 +0900

    refactor: move code - extensions에 존재하였던 코드 일부 이전

--- core-util/src/main/kotlin/io/lemoncloud/core/util/extension/FlowExtensions.kt
+++ core-util/src/main/kotlin/io/lemoncloud/core/util/extension/FlowExtensions.kt
@@ -1,31 +1,10 @@
 package io.lemoncloud.core.util.extension
 
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.combineTransform
-import kotlinx.coroutines.flow.flow
-import java.util.concurrent.TimeUnit
 
 object FlowExtensions {
-
-    /**
-     * 일정 시간 간격으로 틱을 발생시키는 Flow를 생성합니다.
-     * @param interval 시간 간격
-     * @param timeUnit 시간 단위 (ex. MILLISECONDS, SECONDS, MINUTES)
-     * @param tickCount 틱 발생 개수 (기본값: Long.MAX_VALUE)
-     * @return current Timestamp and current Count
-     */
-    fun tickFlow(
-        interval: Long, timeUnit: TimeUnit, tickCount: Long = Long.MAX_VALUE
-    ): Flow<Pair<Long, Long>> = flow {
-        var currentCount = 0L
-        while (tickCount > currentCount) {
-            emit(System.currentTimeMillis() to currentCount)
-            delay(timeUnit.toMillis(interval))
-            currentCount++
-        }
-    }
 
     /**
      * 다수의 Flow를 결합하는 익스텐션
