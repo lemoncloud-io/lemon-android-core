c737924c141ad2c8639b49dcca77ba02fe4a2a61
raine-lemon <raine@lemoncloud.io>
2025-02-19 14:28:51 +0900

    refactor: update comment

--- core-architecture/src/main/kotlin/io/lemoncloud/core/architecture/domain/FlowUseCase.kt
+++ core-architecture/src/main/kotlin/io/lemoncloud/core/architecture/domain/FlowUseCase.kt
@@ -5,8 +5,10 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 
 /**
+ * [FlowUseCase]
  * - 도메인 레이어의 유즈 케이스를 처리합니다.
  * - [Flow] 형태로 반환됩니다.
+ * - [execute] 를 구현하여 `UseCase`를 생성할 수 있습니다.
  */
 abstract class FlowUseCase<In, Out>(private val coroutineDispatcher: CoroutineDispatcher) {
 
